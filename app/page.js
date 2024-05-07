e4d18172fed86b2369bfd7ac0af632b62f0b22de
Gaurav Mahajan <142982401+gaurv4@users.noreply.github.com>
2024-05-07 19:58:31 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -93,7 +93,7 @@ export default function page() {
                 />
                 <button
                   className="absolute top-10 right-6 text-xl"
-                  onClick={pass}
+                  onClick={setshowpass(!showpass)}
                 >
                   {showpass ? (
                     <AiFillEye className="dark:text-white" />
@@ -126,7 +126,7 @@ export default function page() {
                 </a>
               </div>
               <button
-                onclick={sendData}
+                onClick={sendData}
                 className="w-full text-white bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Sign in
