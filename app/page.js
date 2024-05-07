e4fa8249196fe3bf474f8fa9e0cfffbb63296035
Gaurav Mahajan <142982401+gaurv4@users.noreply.github.com>
2024-05-07 19:51:26 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -140,7 +140,7 @@ export default function page() {
                   Sign up
                 </a>
               </p>
-            
+            </div>
           </div>
         </div>
       </div>
